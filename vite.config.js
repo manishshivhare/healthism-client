13873a77a724c8c862e2b250a4a460ef46816db9
Manish Shivhare <67515371+manishshivhare@users.noreply.github.com>
2025-02-18 16:34:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-  proxy: {
-    "/api": {
-      target: "https://ju4m76xqr1.execute-api.eu-north-1.amazonaws.com/v1",
-      changeOrigin: true,
-      secure: true,
-      configure: (proxy, _options) => {
-        proxy.on('error', (err, _req, _res) => {
-          console.log('proxy error', err);
-        });
-        proxy.on('proxyReq', (proxyReq, req, _res) => {
-          console.log('Sending Request:', req.method, req.url);
-        });
-        proxy.on('proxyRes', (proxyRes, req, _res) => {
-          console.log('Received Response:', proxyRes.statusCode, req.url);
-        });
-      },
-    },
-  },
-},
+  
 });
