4ebd656f24dee1c0af36cfdca1c9a7e528dfb762
Manish Shivhare <67515371+manishshivhare@users.noreply.github.com>
2025-03-26 14:33:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://ju4m76xqr1.execute-api.eu-north-1.amazonaws.com/v1',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+
   
 });
